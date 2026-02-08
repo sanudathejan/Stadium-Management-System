421e130080f7ed1470546a0ebdfe72618e62cf86
sanudathejan <thejansanuda2006@gmail.com>
2026-02-08 12:12:31 +0530

    Add components barrel export file
    Create Frontend/src/components/index.js to re-export common UI components (Button, Input, EventCard, Seat, SeatLegend, FoodCard, BookingCard, Header). This provides a single import entrypoint for component consumers and simplifies import paths across the frontend.

---
+++ Frontend/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as EventCard } from './EventCard';
+export { Seat, SeatLegend } from './Seat';
+export { default as FoodCard } from './FoodCard';
+export { default as BookingCard } from './BookingCard';
+export { default as Header } from './Header';
